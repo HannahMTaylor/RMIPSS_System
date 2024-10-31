a12af500f438ce6c30339a4972405687b9906da5
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2024-10-31 19:19:32 -0400

    begin populating entities/models and views

---
+++ RMIPSS_System/Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace RMIPSS_System.Models.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+
+    // will also need to set up roles for either Teacher or State Level Employee
+    // if Teacher: needs a School attribute -- could set "State" as a type of school affiliation for now
+    public string Affiliation { get; set; } //use this (for now?) to determine which school a teacher is with or if State employee
+
+
+}
