09087edb577304b55176fb4d3e4f460874a58b31
sangam <sangam.shrestha1997@gmail.com>
2024-11-08 18:06:38 -0500

    reverting the overriden code

--- RMIPSS_System/Controllers/HomeController.cs
+++ RMIPSS_System/Controllers/HomeController.cs
@@ -13,31 +13,6 @@ namespace RMIPSS_System.Controllers
             _logger = logger;
         }
 
-        [HttpPost]
-        public ActionResult HandleButtonClick(string buttonClicked)
-        {
-            if (buttonClicked == "Refer")
-            {
-                //return Content("Refer button clicked");
-                return RedirectToAction("Referral");
-            }
-                
-            else if (buttonClicked == "Login")
-            {
-                //return Content("Login button clicked.");
-                return RedirectToAction("Index");
-                //return RedirectToAction("Login");
-            }
-
-
-            else
-            {
-                //return Content("No button matched.");
-                return RedirectToAction("Index");
-            }
-                
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -53,9 +28,6 @@ namespace RMIPSS_System.Controllers
             return View();
         }
         
-        
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
