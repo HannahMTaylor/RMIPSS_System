e636bb4826e8a965f8a5ec3cfe50a4d5014420d7
Amit Maharjan <maharjanamit8@gmail.com>
2024-11-08 22:01:16 -0500

    Authentication Enabled and Login Page Redesigned

--- RMIPSS_System/Program.cs
+++ RMIPSS_System/Program.cs
@@ -43,7 +43,7 @@ public class Program
         app.UseStaticFiles();
 
         app.UseRouting();
-
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllerRoute(
