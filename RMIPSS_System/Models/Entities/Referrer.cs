0d328b0d2685e4a51a863ff10031e342efd5096c
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2024-11-14 18:55:20 -0500

    add Referrer and update Employee Models

---
+++ RMIPSS_System/Models/Entities/Referrer.cs
@@ -0,0 +1,13 @@
+﻿namespace RMIPSS_System.Models.Entities;
+
+public class Referrer
+{
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string RelationshipToStudent { get; set; }
+    public Student Student { get; set; }
+    public string Phone {  get; set; }
+    public string Email { get; set; }
+    public DateOnly DateFilledReferral { get; set; }
+}
---
+++ RMIPSS_System/Models/ProcessSteps/Referral.cs
@@ -0,0 +1,29 @@
+﻿using RMIPSS_System.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace RMIPSS_System.Models.ProcessSteps;
+
+public class Referral
+{
+    public int ID {  get; set; }
+    public Student Student { get; set; }
+    public List<string> ReasonsForReferral { get; set; }
+    public IFormFile? MIDScoringSheet { get; set; }
+
+    [MaxLength(560)]
+    public string AreasOfConcernAndHelpNeededDescription { get; set; }
+    public Referrer Referrer { get; set; }
+    public DateOnly ReferralReceived {  get; set; }
+    public DateOnly TeamRecommendation {  get; set; }
+    public DateOnly DispositionNoticeToReferrer {  get; set; }
+    public DateOnly ParentalConsentForEvaluation {  get; set; }
+    public DateOnly EvaluationTeamRecommendation {  get; set; }
+    public DateOnly ParentNoticeForMeeting {  get; set; }
+    public DateOnly ReferredToChildStudyTeam {  get; set; }
+    public string Disposition {  get; set; }
+    public DateOnly DispositionNoticeToParent {  get; set; }
+    public DateOnly ReferralToEvaluationTeam {  get; set; }
+    public string Recommendation { get; set; }
+    public DateOnly IEPMeeting {  get; set; }
+
+}
