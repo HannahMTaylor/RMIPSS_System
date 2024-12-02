ef811bb2733476942806f4c5f3574d2c6d342264
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2024-12-02 08:48:12 -0500

    pushing latest changes to Employee, ReferrerPerson, and Referral Models to feature branch

--- RMIPSS_System/Models/ProcessSteps/Referral.cs
+++ RMIPSS_System/Models/ProcessSteps/Referral.cs
@@ -8,6 +8,7 @@ public class Referral
     public int ID {  get; set; }
     public Student Student { get; set; }
     public List<string> ReasonsForReferral { get; set; }
+    
     //public IFormFile? MIDScoringSheet { get; set; }
 
     [MaxLength(560)]
@@ -20,6 +21,7 @@ public class Referral
     public DateOnly EvaluationTeamRecommendation {  get; set; }
     public DateOnly ParentNoticeForMeeting {  get; set; }
     public DateOnly ReferredToChildStudyTeam {  get; set; }
+    
     public string Disposition {  get; set; }
     public DateOnly DispositionNoticeToParent {  get; set; }
     public DateOnly ReferralToEvaluationTeam {  get; set; }
