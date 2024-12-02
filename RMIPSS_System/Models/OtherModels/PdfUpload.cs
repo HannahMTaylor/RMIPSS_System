0272fb9d9c72c737cb550d14f7f7014047c4c531
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2024-12-02 15:51:18 -0500

    saving progress - BROKEN! Doesn't Compile

---
+++ RMIPSS_System/Models/OtherModels/PdfUpload.cs
@@ -0,0 +1,10 @@
+﻿namespace RMIPSS_System.Models.OtherModels;
+
+public class PdfUpload
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public byte[]? Data { get; set; }
+    public int Length { get; set; }
+    public string ContentType { get; set; } = string.Empty;
+}
--- RMIPSS_System/Models/ProcessSteps/Referral.cs
+++ RMIPSS_System/Models/ProcessSteps/Referral.cs
@@ -1,4 +1,5 @@
 ﻿using RMIPSS_System.Models.Entities;
+using RMIPSS_System.Models.OtherModels;
 using System.ComponentModel.DataAnnotations;
 
 namespace RMIPSS_System.Models.ProcessSteps;
@@ -9,7 +10,7 @@ public class Referral
     public Student Student { get; set; }
     public List<string> ReasonsForReferral { get; set; }
     
-    //public IFormFile? MIDScoringSheet { get; set; }
+    public PdfUpload? MIDScoringSheet { get; set; }
 
     [MaxLength(560)]
     public string AreasOfConcernAndHelpNeededDescription { get; set; }
