752eedd2730f533f65a2585cc8a5ae650ea75ede
Amit Maharjan <maharjanamit8@gmail.com>
2024-12-08 13:46:17 -0500

    Revert "pushing latest changes to Employee, ReferrerPerson, and Referral Models to feature branch"
    This reverts commit ef811bb2733476942806f4c5f3574d2c6d342264.

--- RMIPSS_System/Models/Entities/Employee.cs
+++ RMIPSS_System/Models/Entities/Employee.cs
@@ -1,20 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RMIPSS_System.Models.Entities;
+﻿namespace RMIPSS_System.Models.Entities;
 
 public class Employee
 {
     public int Id { get; set; }
-    [MaxLength(50)]
     public string LastName { get; set; }
-    [MaxLength(50)]
     public string FirstName { get; set; }
 
     // will also need to set up roles for either Teacher or State Level Employee
     // if Teacher: needs a School attribute -- could set "State" as a type of school affiliation for now
-    [MaxLength(150)]
     public string Affiliation { get; set; } //use this (for now?) to determine which school a teacher is with or if State employee
-    [MaxLength(150)]
+
     public string? TeachingLicenseType { get; set; }
 
 }
