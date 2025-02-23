e5c3a7f2b1f444ad6155a521a75710122569e6a9
sangam <sangam.shrestha1997@gmail.com>
2025-02-23 14:41:50 -0500

    fixed user repository issues

--- RMIPSS_System/Repository/IRepository/IApplicationUserRepository.cs
+++ RMIPSS_System/Repository/IRepository/IApplicationUserRepository.cs
@@ -9,6 +9,5 @@ public interface IApplicationUserRepository : IRepository<ApplicationUser>
     Task<ApplicationUser> CreateApplicationUserAsync(ApplicationUser applicationUser, string password);
     Task AssignUserToRoleAsync(string userName, string rolename);
     Task<bool> DeleteUserAsync(String username);
-
-    Task<ApplicationUser?> GetApplicationUserAsync(String userName);
+    
 }
