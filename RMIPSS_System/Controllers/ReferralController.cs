51ce048a241f029385a275325bcf385eda18ef84
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2025-02-24 11:03:22 -0500

    partial update on Controller and Service classes to save/submit a referral form and view existing referrals by student id

--- RMIPSS_System/Controllers/ReferralController.cs
+++ RMIPSS_System/Controllers/ReferralController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RMIPSS_System.Models.ViewModel;
+using RMIPSS_System.Models.ProcessSteps;
 using RMIPSS_System.Repository.IRepository;
 using RMIPSS_System.Services;
+using Microsoft.AspNetCore.Authorization;
+using RMIPSS_System.Models.Entities;
 
 namespace RMIPSS_System.Controllers;
 
 /// <summary>
-/// The controller handles basic CRUD functionality and will utilize the repository and service classes for the referral process step
+/// The controller handles basic CRUD functionality and will utilize the repository and service classes for the referralVM process step
 /// </summary>
+/// 
+
+
 public class ReferralController : Controller
 {
     private readonly ILogger<ReferralController> _logger;
@@ -21,11 +28,69 @@ public class ReferralController : Controller
     }
 
 
-    //create new referral
+    //create new referralVM (on submit button press)
+    //need to add some sort of functionality to save incrementally?? might add in next sprint
+    //
+    //*** need to go back through and make sure Student and Referrer are getting created and mapped properly to Referral on submission
+    public async Task<IActionResult> SubmitReferralForm(ReferralViewModel referralVM)
+    {
+        try
+        {
+            //use service class to convert view model to model and pass model to controllers
+            Referral refer = await _referralService.ConvertViewModel(referralVM);
 
-    //load/read existing referral
+            //then update database
+            await _referralRepo.SaveReferralAsync(refer);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating referral form. Please try again later");
+            
+        }
 
-    //update existing referral
+        return RedirectToAction("Error", "Home");
+    }
 
-    //delete existing referral
+    //load/read existing referralVM -- needs logged in user permissions
+    [Authorize(Roles = Constants.ROLE_STATE_AND_SCHOOL_USER)]
+    public async Task<IActionResult> ViewExistingReferralByStudentId([Bind(Prefix = "id")] int studentId)
+    {
+        try
+        {
+            Student student = await _referralService.GetStudentById(studentId);
+            if (student == null)
+            {
+                _logger.LogError($"Error retrieving Student record with id: {studentId} from student database. Please try again later.");
+                return RedirectToAction("Error", "Home");
+            }
+
+            //if student is valid... continue fetching referral data
+            Referral referralEntForStudent = await _referralService.GetReferralDataByStudentId(studentId);
+            if (referralEntForStudent != null)
+            {
+                ReferralViewModel referralViewModel = new ReferralViewModel
+                {
+                    student = student,
+                    person = referralEntForStudent.Referrer!,
+                    referral = referralEntForStudent
+                };
+                return View(referralViewModel);
+            }
+            else
+            {
+                _logger.LogError($"Referral form does not exist for student with id: {studentId}");
+                return RedirectToAction("Error", "Home");
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error loading Referral for student with id: {studentId}");
+            return RedirectToAction("Error", "Home");
+        }
+    }
+
+    //update existing referralVM -- needs logged in user permissions
+
+    //delete existing referralVM -- needs logged in user permissions
 }
--- RMIPSS_System/Services/ReferralService.cs
+++ RMIPSS_System/Services/ReferralService.cs
@@ -1,14 +1,56 @@
-﻿namespace RMIPSS_System.Services;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RMIPSS_System.Models.Entities;
+using RMIPSS_System.Models.ProcessSteps;
+using RMIPSS_System.Models.ViewModel;
+using RMIPSS_System.Repository.IRepository;
+
+namespace RMIPSS_System.Services;
 /// <summary>
 /// The service class is used to get logged in user info, student info, and other necessary information or system information to load pages from the controller.
+/// 
+/// This is for use of viewing/editing existing referral forms by student id which should be the only time the referral process requires a logged in user.
+/// 
+/// Creating a new referral should be accessible by anyone.
 /// </summary>
 public class ReferralService
 {
+    private readonly ILogger<ReferralService> _logger;
+    private readonly IStudentRepository _studentRepo;
+    private readonly IApplicationUserRepository _appUserRepo;
+
+
     //get logged in user
 
     //get student id - if applicable
+    public async Task<Student> GetStudentById(int studentId)
+    {
+        throw new NotImplementedException();
+    }
 
-    //get referral id (if null - create new referral)
+    //get referral id/data for view existing referrrals (if null - create new referral? or redirect to home page so they can create one)
+    public async Task<Referral> GetReferralDataByStudentId(int studentId)
+    {
+        throw new NotImplementedException();
+    }
 
-    //get referral data
+    //map viewmodel to referral data
+    public Task<Referral> ConvertViewModel(ReferralViewModel referralVM)
+    {
+        Referral referralEntity = new();
+        try
+        {
+            referralEntity = referralVM.referral;
+            return Task.FromResult(referralEntity);
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogError(ex, "Error converting view model to model");
+            throw;
+        }
+
+    }
+
+    
+
+    
 }
