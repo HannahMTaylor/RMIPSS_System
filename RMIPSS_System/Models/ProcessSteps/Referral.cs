5505192e93da2dc857246896295ae0c853f22bb6
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2025-02-24 04:54:36 -0500

    fix suggestions in Referral model

--- RMIPSS_System/Models/ProcessSteps/Referral.cs
+++ RMIPSS_System/Models/ProcessSteps/Referral.cs
@@ -8,20 +8,23 @@ public class Referral
 {
     public int ID {  get; set; }
 
+    public int StudentId { get; set; }
+
     //section 1 of form
-    public Student Student { get; set; }
+    public Student? Student { get; set; }
 
     //section two of form
-    public List<string> ReasonsForReferral { get; set; }
+    public string ReasonsForReferral { get; set; } = string.Empty;
     public string? OtherReasonsForReferral { get; set; }
     //this has not been added to Db table Referrals yet bc not working
     // public PdfUpload? MIDScoringSheet { get; set; }
 
     [MaxLength(560)]
-    public string AreasOfConcernAndHelpNeededDescription { get; set; }
+    public string? AreasOfConcernAndHelpNeededDescription { get; set; }
 
     //section 3
-    public ReferrerPerson Referrer { get; set; }
+    public int ReferrerId { get; set; }
+    public ReferrerPerson? Referrer { get; set; }
 
     //section 4: school use only
     public DateOnly ReferralReceived {  get; set; }
@@ -32,10 +35,10 @@ public class Referral
     public DateOnly ParentNoticeForMeeting {  get; set; }
     public DateOnly ReferredToChildStudyTeam {  get; set; }
 
-    public string Disposition {  get; set; }
+    public string? Disposition {  get; set; }
     public DateOnly DispositionNoticeToParent {  get; set; }
     public DateOnly ReferralToEvaluationTeam {  get; set; }
-    public string Recommendation { get; set; }
+    public string? Recommendation { get; set; }
     public DateOnly IEPMeeting {  get; set; }
 
 }
