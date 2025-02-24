ee970aa28038af5174800f8ae604302125c9dc3c
sangam <sangam.shrestha1997@gmail.com>
2025-02-23 19:47:42 -0500

    Implementing api for student details

--- RMIPSS_System/Controllers/StudentController.cs
+++ RMIPSS_System/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RMIPSS_System.Models.ViewModel;
 using RMIPSS_System.Services;
 
 namespace RMIPSS_System.Controllers;
@@ -8,14 +9,53 @@ namespace RMIPSS_System.Controllers;
 public class StudentController : Controller
 {
     private readonly ILogger<StudentController> _logger;
+    private readonly StudentService _studentService;
 
-    public StudentController(ILogger<StudentController> logger)
+    public StudentController(ILogger<StudentController> logger, StudentService studentService)
     {
         _logger = logger;
+        _studentService = studentService;
     }
     
     public IActionResult ListStudent()
     {
         return View();
     }
+    
+    public async Task<IActionResult>  GetStudentDetailsById(int studentId)
+    {
+        if (studentId == null || studentId == 0)
+        {
+            TempData["error"] = "Select Student";
+            return RedirectToAction("Index","Home");
+        }
+
+        try
+        {
+            StudentViewModel studentViewModel = await _studentService.GetStudentByIdAsync(studentId);
+            if (studentViewModel != null)
+            {
+                return View(studentViewModel);
+            }
+            else
+            {
+                _logger.LogError("Error while viewing details of " + studentViewModel.FirstName + " " + studentViewModel.LastName);
+                TempData["error"] = "Error: Student Details is not loading, Please try again.";
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An exception occurred while adding a user.");
+            Console.WriteLine($"Exception occurred: {ex.Message}");
+            Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+            TempData["error"] = "An unexpected error occurred. Please try again.";
+        } 
+        
+        return RedirectToAction("Index","Home");
+
+
+    }
+            
+    
 }
