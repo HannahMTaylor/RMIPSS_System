dfb7ab50fb0dadae2de93c306d9daaacb0e58e8e
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2025-03-24 09:57:13 -0400

    referral testing file

---
+++ RMIPSS_System_UnitTest/ReferralTests/AReferralRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using RMIPSS_System.Models.Entities;
+using RMIPSS_System.Repository;
+using RMIPSS_System_UnitTest.Common;
+
+namespace RMIPSS_System_UnitTest.ReferralTests;
+
+public class AReferralRepository
+{
+    [Test]
+    public void ShouldSaveReferralsToTheDatabase()
+    {
+        //Arrange
+        Student testStudent = new Student
+        {
+            FirstName = "Test",
+            LastName = "TestLast",
+            Email = "TestEmail@gmail.com"
+        };
+        Repositories._studentRepository.Save(testStudent);
+
+        DateOnly testDate = new DateOnly(2025, 3, 24);
+
+        ReferrerPerson testPerson = new ReferrerPerson
+        {
+            FullName = "Test",
+            Phone = "1234567890",
+            Email = "1234567890@mail.com",
+            DateFilledReferral = testDate
+        };
+        Repositories._refPersonRepo.Save(testPerson);
+
+        ReferralRepository sut = new ReferralRepository();
+
+        //Act
+
+        //Assert
+    }
+}
