24ab62f7231a76723d89fbfbab566c8f750cc5cf
Amit Maharjan <maharjanamit8@gmail.com>
2025-03-30 14:33:28 -0400

    Added option to use the application as a progressive web application.

---
+++ RMIPSS_System/wwwroot/serviceworker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "RMIPSS-CACHE-V1";
+const urlsToCache = [
+    "/",
+    "/css/site.css",
+    "/js/site.js",
+    "/offline.html"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        }).catch(() => {
+            return caches.match("/offline.html");
+        })
+    );
+});
+
+// Activate service worker and remove old caches
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
